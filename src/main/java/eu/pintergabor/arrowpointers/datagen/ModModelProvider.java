ef059cd0b8b88b114a825912e307b7fb3e1152f9
Pintér Gábor <pinter.gabor@gmx.at>
2024-12-13 17:33:02 +0100

    1.0.5+mc1.21.4

--- src/main/java/eu/pintergabor/arrowpointers/datagen/ModModelProvider.java
+++ src/main/java/eu/pintergabor/arrowpointers/datagen/ModModelProvider.java
@@ -1,26 +1,24 @@
 package eu.pintergabor.arrowpointers.datagen;
 
 import eu.pintergabor.arrowpointers.main.ArrowRegistry;
-
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.data.client.BlockStateModelGenerator;
-import net.minecraft.data.client.ItemModelGenerator;
-
+import net.minecraft.client.data.BlockStateModelGenerator;
+import net.minecraft.client.data.ItemModelGenerator;
 
 public class ModModelProvider extends FabricModelProvider {
-	public ModModelProvider(FabricDataOutput output) {
-		super(output);
-	}
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
 
-	@Override
-	public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-		ModModelGenerator generator = new ModModelGenerator(blockStateModelGenerator);
-		generator.registerFlat9Direction(ArrowRegistry.arrowMarkBlock);
-		generator.registerFlat9Direction(ArrowRegistry.glowArrowMarkBlock);
-	}
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        ModModelGenerator generator = new ModModelGenerator(blockStateModelGenerator);
+        generator.registerFlat9Direction(ArrowRegistry.arrowMarkBlock);
+        generator.registerFlat9Direction(ArrowRegistry.glowArrowMarkBlock);
+    }
 
-	@Override
-	public void generateItemModels(ItemModelGenerator itemModelGenerator) {
-	}
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+    }
 }
